175b0f7cda84b866da4e0462c301861943f649fe
Roger <12768403+rogix@users.noreply.github.com>
2019-07-09 16:55:29 -0300

    chores: create components header and index

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Home from './components/Index';
+
+function App() {
+	return (
+		<div className="app-container">
+			<Home />
+		</div>
+	);
+}
+
+export default App;
